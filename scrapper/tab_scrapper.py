a5803e755d223b4ec840dcf3574b7dbc05c05f38
kevinszuchet <kevinszuchet@gmail.com>
2021-07-05 16:17:49 +0300

    initial structure for the details tab scrapper implementation

---
+++ scrapper/tab_scrapper.py
@@ -0,0 +1,30 @@
+class TabScrapper:
+    def get_information(self):
+        pass
+
+
+class KeyValueTabScrapper(TabScrapper):
+    def get_information(self):
+        pass
+
+
+class ScoreTabScrapper(KeyValueTabScrapper):
+    def get_information(self):
+        pass
+
+
+class DigitalNomadGuideTabScrapper(KeyValueTabScrapper):
+    def get_information(self):
+        pass
+
+
+class CostOfLivingTabScrapper(KeyValueTabScrapper):
+    def get_information(self):
+        pass
+
+
+class ProsAndCons(TabScrapper):
+    def get_information(self):
+        pass
+
+# TODO RemoteJobs?, Reviews?, Photos?
