4936678d8cacbe404885ab094000fdd46c54957e
kevinszuchet <kevinszuchet@gmail.com>
2021-07-05 15:21:31 +0300

    work with futures to get all the city details making a get to each page

--- scrapper/nomad_list_scrapper.py
+++ scrapper/nomad_list_scrapper.py
@@ -1,9 +1,11 @@
 import requests
 import time
+from requests_futures.sessions import FuturesSession
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from conf import NOMAD_LIST_URL, NOMAD_LIST_SCROLL_PAUSE_TIME
 from scrapper.city_scrapper import CityScrapper
+from concurrent.futures import as_completed
 
 
 # TODO use logging
@@ -15,15 +17,10 @@ class NomadListScrapper:
     def __init__(self):
         self._baseUrl = NOMAD_LIST_URL
         self._driver = webdriver.Chrome()
+        # max_workers default value 8
+        self._session = FuturesSession()
 
-    def get_cities(self):
-        """
-        Takes the cities from the home page, builds a dictionary for each one with the available information.
-        Then, returns a list of dicts with all the cities.
-        """
-        return [CityScrapper().get_city_information(city_li) for city_li in self.get_all_the_cities()]
-
-    def get_all_the_cities(self):
+    def _get_all_the_cities(self):
         self._driver.get(self._baseUrl)
         scroll_height = self._get_scroll_height()
 
@@ -66,3 +63,17 @@ class NomadListScrapper:
     def _get_scroll_height(self):
         """Takes the scroll height of the document executing javascript in the browser."""
         return self._driver.execute_script("return document.body.scrollHeight")
+
+    def _get_city_details(self, city_li):
+        """Given the city li, takes the endpoint from the CityScrapper, and returns the result of making the request."""
+        url = f"{self._baseUrl}{CityScrapper.get_city_url(city_li)}"
+        return self._session.get(url)
+
+    def get_cities(self):
+        """
+        Takes the cities from the home page, builds a dictionary for each one with the available information.
+        Then, returns a list of dicts with all the cities.
+        """
+        futures = (self._get_city_details(city_li) for city_li in self._get_all_the_cities())
+        # TODO handle errors
+        return [CityScrapper().get_city_details(future.result().content) for future in as_completed(futures)]
