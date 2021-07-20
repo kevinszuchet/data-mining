c018f77684afbe543826d2db96bf44f51d266cb8
kevinszuchet <kevinszuchet@gmail.com>
2021-07-20 11:04:57 +0300

    use the generic conf outside scrapper folder + webdriver path in conf file

--- conf.py
+++ conf.py
@@ -1,4 +1,8 @@
 """ Config file with parameters. """
 
+import os
+
 NOMAD_LIST_URL = "https://nomadlist.com"
 NOMAD_LIST_SCROLL_PAUSE_TIME = 10
+
+PATH_TO_WEB_DRIVER = os.getenv('PATH_TO_WEB_DRIVER')
--- scrapper/conf.py
+++
@@ -1,4 +0,0 @@
-""" Config file with parameters. """
-
-NOMAD_LIST_URL = "https://nomadlist.com"
-NOMAD_LIST_SCROLL_PAUSE_TIME = 10
