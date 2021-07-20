dfde1ad4ee68799e4de3753c29662e07802477ba
kevinszuchet <kevinszuchet@gmail.com>
2021-07-20 16:11:10 +0300

    add some logs

--- scrapper/city_scrapper.py
+++ scrapper/city_scrapper.py
@@ -1,7 +1,8 @@
 import re
-from bs4 import BeautifulSoup
-from scrapper.tab_scrapper import TabScrapper, CostOfLivingTabScrapper, DigitalNomadGuideTabScrapper, \
-    ProsAndConsTabScrapper, ReviewsTabScrapper, ScoresTabScrapper, WeatherTabScrapper
+from scrapper.tab_scrapper import *
+from logger import Logger
+
+# TODO: is it ok if I change a method from static to non-static because of the logger?
 
 
 class CityScrapper:
@@ -11,21 +12,35 @@ class CityScrapper:
     # To avoid cities lis with 'data-slug="{slugName}"'
     city_template_re = re.compile(r'{\w+}')
 
-    @staticmethod
-    def valid_tag(city_li):
-        """Given the city li, checks if the tag is valid."""
-        attrs = city_li.attrs
-        return attrs.get('data-type') == 'city' and not CityScrapper.city_template_re.search(attrs.get('data-slug'))
-
-    @staticmethod
-    def get_city_url(city_li):
-        """Given the city li, returns the url of it to go to the details."""
-        a = city_li.find("a", attrs={'itemprop': 'url'})
-        return a.attrs.get("href").strip()
+    def __init__(self):
+        self._logger = Logger().logger
 
     def _get_tab_information(self, tab, city_details_soup):
+        self._logger.info("Getting the tab information...")
+        self._logger.debug(f"Tab: {tab}")
         tab_name = TabScrapper.tab_name(tab)
-        return eval(f"{tab_name}TabScrapper")(city_details_soup).get_information()
+        self._logger.debug(f"Tab name: {tab_name}")
+        dynamic_tab_scrapper = eval(f"{tab_name}TabScrapper")
+        self._logger.debug(f"DynamicTabScrapper: {dynamic_tab_scrapper}")
+        return dynamic_tab_scrapper(city_details_soup).get_information()
+
+    def valid_tag(self, city_li):
+        """Given the city li, checks if the tag is valid."""
+        self._logger.debug(f"Validating the city li tag: {city_li}")
+        if city_li is None:
+            return False
+
+        attrs = city_li.attrs
+        self._logger.debug(f"Here they are... The city li attributes: {attrs}")
+        return attrs.get('data-type') == 'city' and not CityScrapper.city_template_re.search(attrs.get('data-slug'))
+
+    def get_city_url(self, city_li):
+        """Given the city li, returns the url of it to go to the details."""
+        a = city_li.find("a", attrs={'itemprop': 'url'})
+        self._logger.debug(f"City li > a: {a}")
+        if a:
+            self._logger.debug(f"a > attrs: {a.attrs}")
+            return a.attrs.get("href").strip()
 
     def get_city_details(self, city_details_html):
         """
--- scrapper/nomad_list_scrapper.py
+++ scrapper/nomad_list_scrapper.py
@@ -8,9 +8,10 @@ from bs4 import BeautifulSoup
 from conf import NOMAD_LIST_URL, NOMAD_LIST_SCROLL_PAUSE_TIME, PATH_TO_WEB_DRIVER
 from scrapper.city_scrapper import CityScrapper
 from concurrent.futures import as_completed
+from logger import Logger
 
 
-# TODO use logging
+# TODO handle errors + logging
 
 
 class NomadListScrapper:
@@ -21,46 +22,46 @@ class NomadListScrapper:
         self._driver = webdriver.Chrome(PATH_TO_WEB_DRIVER) if PATH_TO_WEB_DRIVER else webdriver.Chrome()
         # max_workers default value 8
         self._session = FuturesSession()
+        self._logger = Logger().logger
 
     def _get_all_the_cities(self):
         """Scroll to the end of the main page fetching all the cities as li tags."""
         self._driver.get(self._baseUrl)
         scroll_height = self._get_scroll_height()
 
-        print("Initial scroll height:", scroll_height)
+        self._logger.info("Initial scroll height:", scroll_height)
 
         while True:
             try:
                 # Scroll down to bottom
-                print("Scroll down to the bottom...")
+                self._logger.info("Scroll down to the bottom...")
                 self._driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-                print(f"Time to sleep, see you in {NOMAD_LIST_SCROLL_PAUSE_TIME} seconds...")
+                self._logger.info(f"Time to sleep, see you in {NOMAD_LIST_SCROLL_PAUSE_TIME} seconds...")
 
                 # Wait to load page
                 time.sleep(NOMAD_LIST_SCROLL_PAUSE_TIME)
 
                 # Calculate new scroll height and compare with last scroll height
-                print("Getting the new scroll height...")
+                self._logger.info("Getting the new scroll height...")
                 new_scroll_height = self._get_scroll_height()
-                print(f"Old scroll height: {scroll_height} - New scroll height: {new_scroll_height}")
+                self._logger.info(f"Old scroll height: {scroll_height} - New scroll height: {new_scroll_height}")
 
-                print(f"Checking if scroll heights {new_scroll_height} == {scroll_height}")
+                self._logger.info(f"Checking if scroll heights {new_scroll_height} == {scroll_height}")
                 if new_scroll_height == scroll_height:
                     # If scroll heights are the same, it'll break the loop
                     break
 
                 scroll_height = new_scroll_height
             except Exception as e:
-                print("Error trying to scroll to the end!", e)
-                print("Getting all the cities fetched until now...")
+                self._logger.error("Error trying to scroll to the end!", e)
+                self._logger.info("Getting all the cities fetched until now...")
                 break
-                # raise e
 
         page_source = self._driver.page_source
-        # This is only for debugging
         soup = BeautifulSoup(page_source, "html.parser")
-        # print("This is the pretty page source:", soup.prettify())
+        self._logger.debug("This is the pretty page source:", soup.prettify())
+        self._driver.close()
         return soup.find_all('li', attrs={'data-type': 'city'})
 
     def _get_scroll_height(self):
@@ -69,15 +70,20 @@ class NomadListScrapper:
 
     def _do_request(self, city_li):
         """Given the city li, takes the endpoint from the CityScrapper, and returns the result of making the request."""
-        url = f"{self._baseUrl}{CityScrapper.get_city_url(city_li)}"
-        return self._session.get(url)
+        url = f"{self._baseUrl}{CityScrapper().get_city_url(city_li)}"
+        html = self._session.get(url)
+        self._logger.info(f"Request to {url} made, now is time to sleep...")
+        time.sleep(5)
+        return html
 
     def _make_request_to_city_details(self):
         """Checks if the lis are valid, takes the valid ones and make the requests to the city details page."""
-        return (self._do_request(li) for li in self._get_all_the_cities() if CityScrapper.valid_tag(li))
+        return (self._do_request(li) for li in self._get_all_the_cities() if CityScrapper().valid_tag(li))
 
     def _get_city_details(self, future):
-        city_details_html = future.result().content
+        res = future.result()
+        self._logger.debug(f"City Details GET - Status Code: {res.status_code}")
+        city_details_html = res.content
         return CityScrapper().get_city_details(city_details_html)
 
     def get_cities(self):
@@ -87,8 +93,6 @@ class NomadListScrapper:
         """
         try:
             futures = self._make_request_to_city_details()
-            # TODO handle errors
             return [self._get_city_details(future) for future in as_completed(futures)]
         except Exception as e:
-            print(e)
-            self._driver.close()
+            self._logger.error(f"Exception: {e}")
