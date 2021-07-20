450ad0fb18b0b6f98ec537530b1fd99991ed792d
kevinszuchet <kevinszuchet@gmail.com>
2021-07-20 16:11:02 +0300

    more config props for the logger

--- conf.py
+++ conf.py
@@ -6,3 +6,6 @@ NOMAD_LIST_URL = "https://nomadlist.com"
 NOMAD_LIST_SCROLL_PAUSE_TIME = 10
 
 PATH_TO_WEB_DRIVER = os.getenv('PATH_TO_WEB_DRIVER')
+
+LOG_FILE = "logs.log"
+LOG_FORMAT = '%(asctime)s-%(levelname)s-FILE:%(filename)s-FUNC:%(funcName)s-LINE:%(lineno)d-%(message)s'
