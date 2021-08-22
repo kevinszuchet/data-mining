de0d7ad44d8581db12762f3e818e2239eb0f7994
kevinszuchet <kevinszuchet@gmail.com>
2021-08-22 14:32:40 +0300

    avoid side effect in the nomad list scrapper

--- scrapper/nomad_list_scrapper.py
+++ scrapper/nomad_list_scrapper.py
@@ -16,82 +16,73 @@ class NomadListScrapper:
         self._driver = web_driver
         self._logger = logger
 
-        # Information after scrapping
-        self._cities = []
-
         # Beautiful soup
         self._cities_lis = None
 
-        # TODO ???????
-        # David's
-        self._cities_details = None
         self._city_scrapper = CityScrapper(self._logger)
-        self.page = None
-        self.get_cities()
 
-    def scrap_cities(self):
-        return self._cities
-
-    def load_html_from_disk(self):
+    def _load_html_from_disk(self):
         """Attempts to load the html locally"""
         try:
             with open(CFG.PAGE_SOURCE, 'r') as opened_file:
-                self.page = opened_file.read()
+                return opened_file.read()
         except Exception as e:
             self._logger.error(f'There was an error loading the html file on path : {CFG.PAGE_SOURCE}. Error: {e}')
             sys.exit(1)
 
-    def write_html_to_disk(self):
+    def _write_html_to_disk(self, page_source):
         try:
             with open(CFG.PAGE_SOURCE, 'w+') as opened_file:
-                opened_file.write(self.page)
+                opened_file.write(page_source)
         except Exception as e:
             self._logger.error(f'There was an error writing the html file on path : {CFG.PAGE_SOURCE}. Error: {e}')
             sys.exit(1)
 
-    def fetch_website(self):
+    def _fetch_website(self):
         """Fetches the main website HTML"""
         # Scroll to the end to add more cities
         if CFG.SCROLL:
             self._driver.scroll_to_the_end()
-        self.page = self._driver.get_page_source()
+        return self._driver.get_page_source()
 
     def _get_html(self):
         """Gets the Main HTML file which contents will be scrapped"""
         self._logger.info('Retrieving base Html file')
-        if os.path.exists(CFG.PAGE_SOURCE) and os.getenv('ENV') != "production" and CFG.LOAD_HTML_FROM_DISK:
-            self.load_html_from_disk()
+        should_fetch_and_dump_from_disk = os.path.exists(CFG.PAGE_SOURCE) and os.getenv('ENV') != "production" and CFG.LOAD_HTML_FROM_DISK
+        if should_fetch_and_dump_from_disk:
+            self._load_html_from_disk()
         else:
-            self.fetch_website()
-            self.write_html_to_disk()
-            self._logger.info('New Html written to disk')
+            page_source = self._fetch_website()
+            if should_fetch_and_dump_from_disk:
+                self._write_html_to_disk(page_source)
+                self._logger.info('New Html written to disk')
+            return page_source
 
-    def _get_all_the_cities(self):
+    def _get_all_the_cities(self, page_source):
         """Scroll to the end of the main page fetching all the cities as li tags."""
         try:
-            if self.page is None:
+            if page_source is None:
                 self._logger.error('The website is None')
                 return
 
-            soup = BeautifulSoup(self.page, "html.parser")
+            soup = BeautifulSoup(page_source, "html.parser")
             self._logger.debug(f"Created Beautiful soup object from the HTML file")
             cities_lis = soup.find_all('li', attrs={'data-type': 'city'})
             self._logger.debug(f"Cities achieved")
-            self._cities_lis = cities_lis
+            return cities_lis
         except(AttributeError, KeyError) as e:
             self._logger.error(f"Error trying to find all the cities in the page source: {e}")
             sys.exit(1)
 
-    def _make_request_to_city_details(self):
+    def _requests_to_city_details(self, cities_lis):
         """Checks if the lis are valid, takes the valid ones and make the requests to the city details page."""
-        self._logger.debug(f'Number of Cities: {len(self._cities_lis)}')
+        self._logger.debug(f'Number of Cities: {len(cities_lis)}')
         self._logger.debug(f"Fetching more info for the cities.... This might take time.")
         cities_urls = (
             grequests.get(f"{self._base_url}{self._city_scrapper.get_city_url(x)}", headers=CFG.HEADERS, stream=False)
-            for x in self._cities_lis if self._city_scrapper.valid_tag(x))
-        self._cities_details = (grequests.map(cities_urls, size=CFG.NOMAD_LIST_REQUESTS_BATCH_SIZE,
-                                              exception_handler=self._exception_handler))
-        self._logger.debug(f"More details fetched")
+            for x in cities_lis if self._city_scrapper.valid_tag(x))
+        return (grequests.map(cities_urls, size=CFG.NOMAD_LIST_REQUESTS_BATCH_SIZE,
+                              exception_handler=self._exception_handler))
 
     def _get_city_details(self, res):
         city_details_html = res.content
@@ -103,25 +94,23 @@ class NomadListScrapper:
     def _exception_handler(self, req, error):
         self._logger.error(f"Error making the request {req}: {error}")
 
-    def get_cities(self):
+    def scrap_cities(self):
         """
         Takes the cities from the home page, builds a dictionary for each one with the available information.
         Then, returns a list of dicts with all the cities.
         """
-        self._get_html()
-        self._get_all_the_cities()
-        self._make_request_to_city_details()
+        page_source = self._get_html()
+        cities_lis = self._get_all_the_cities(page_source)
 
-        for res in self._cities_details:
+        for res in self._requests_to_city_details(cities_lis):
             try:
                 details = self._get_city_details(res)
                 res.close()
                 if details is None:
                     self._logger.info(f"Nothing to append with this city :(")
                     break
-                self._logger.info(f"Appending new details...")
+                self._logger.info(f"Storing new details...")
                 MySQLConnector.insert_city_info(details)
-                self._cities.append(details)
             except HTTPError as e:
                 self._logger.error(f"HTTPError raised: {e}")
             except Exception as e:
