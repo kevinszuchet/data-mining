a67e58e63edb0cca339a38165a6f97f7975bbae1
kevinszuchet <kevinszuchet@gmail.com>
2021-08-30 12:58:38 +0300

    execute the cli using the main flie

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-import json
 import sys
-
-from scrapper.nomad_list_scrapper import NomadListScrapper
-from conf import JSON_FILENAME, NOMAD_LIST_URL
 from logger import Logger
-from scrapper.web_driver import WebDriver
-from db.mysql_connector import MySQLConnector
-
-
-def run_create_database_schema():
-    logger = Logger().logger
-    mysql_connector = MySQLConnector(logger)
-    MySQLConnector.create_database(mysql_connector)
+from cli.cli import CommandLineInterface
 
 
 def main():
-    logger = Logger().logger
-    web_driver = WebDriver(logger, NOMAD_LIST_URL)
     try:
-        nomad_scrapper = NomadListScrapper(logger, web_driver)
-        nomad_scrapper.scrap_cities()
-        logger.info(f"Finished Scrapping Cities")
+        CommandLineInterface()
     except Exception as e:
-        # exc_info=True to log the traceback
-        logger.error(f"Exception raised: {e}")
+        Logger().logger.error(f"Exception raised: {e}")
         sys.exit(1)
 
 
 if __name__ == "__main__":
-    run_create_database_schema()
     main()
